8bd2275b1ffbd7becb592d8f65fe93e89c940d4d
hemanthkumar <hemanthcse1@gmail.com>
2023-08-16 23:19:24 +0530

    Initial commit

---
+++ base-domains/src/main/java/com/hemanth/basedomains/dto/OrderEvent.java
@@ -0,0 +1,17 @@
+package com.hemanth.basedomains.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderEvent {
+
+
+    private String message;
+    private String status;
+    private Order order;
+
+}
